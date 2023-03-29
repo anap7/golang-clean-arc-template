5c94fdcf1d2c0e24dafbcec17283a9e03f69e84a
Nanzinha <ana.flaviap7@hotmail.com>
2023-03-29 03:21:04 -0300

    feat: create migrations

---
+++ app/adapter/repository/fixture/fixture.go
@@ -0,0 +1,35 @@
+package fixture
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"io/fs"
+	"log"
+	"github.com/maragudk/migrate"
+)
+
+func Up(migrationsDir fs.FS) *sql.DB {
+	ConnectionString := fmt.Sprintf("%s:%s@/%s?charset=utf8&parseTime=True&loc=Local",
+		"golang",
+		"Golang_17396",
+		"orders",
+	)
+
+	db, err := sql.Open("mysql", ConnectionString)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := migrate.Up(context.Background(), db, migrationsDir); err != nil {
+		panic(err) 
+	}
+	return db
+}
+
+func Down(db *sql.DB, migrationsDir fs.FS) {
+	if err := migrate.Down(context.Background(), db, migrationsDir); err != nil {
+		panic(err) 
+	}
+
+	db.Close()
+}
